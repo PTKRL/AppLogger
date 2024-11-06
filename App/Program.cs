5f1e608e5f46a498c7ca0e3042f6ce08db5e8333
Paula Timonen <2201746@edu.karelia.fi>
2024-11-06 11:42:42 +0200

    Add project files.

---
+++ App/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using AppLogger;
+
+namespace App
+{
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             Logger.Log("Tämä on lokitiedote.");
+         }
+     }
+}
